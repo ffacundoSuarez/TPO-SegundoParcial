e9a5a35c5ba14595cff1f8bf99346a53de16ba79
ffacusuarez <suarezfacundo460@gmail.com>
2026-06-05 11:39:22 -0300

    commit FuncionNumero 3

--- funciones.py
+++ funciones.py
@@ -13,5 +13,53 @@ def llenar():
         ["Fernando Alonso", 14, "Aston Martin", 120, 89.50, 1900000.00, 3]
     ]
     
-def eliminar_piloto():
-    
+
+
+def ModificarPuntos_TiempoPromedio(pilotos):
+    """
+    Permite modificar el puntaje o tiempo de los pilotos
+    """
+
+    print("\n  --- Modificar Puntos o Tiempo Promedio  ---")
+
+    # Busqueda del piloto
+    busqueda = int(input("Ingrese el nombre del piloto o el numero de monoplaza a buscar: "))
+
+    id_piloto = -1
+    cantidad_pilotos = len(pilotos)
+    i = 0
+
+    while i < cantidad_pilotos and id_piloto == -1:
+        if pilotos[i][0].lower() == busqueda.lower() or pilotos[i][1] == int(busqueda):
+            id_piloto = i
+        i += 1
+
+    if id_piloto == -1:
+        print("No se encontro ningun piloto con los datos ingresados.")
+    else:
+        piloto = pilotos[id_piloto]
+        opcion = 0
+        while opcion != 3: 
+            print(f"\n=========================================")
+            print(f"Piloto: {piloto[0]} | Monoplaza: #{piloto[1]}")
+            print(f"Valores actuales -> Puntos: {piloto[3]} | Tiempo: {piloto[4]} segs")
+            print(f"=========================================")
+            print("1. Modificar Puntos Acumulados")
+            print("2. Modificar Tiempo Promedio por Vuelta")
+            print("3. Volver al menú principal")
+            print("=========================================")
+            
+            opcion = int(input("Seleccione una opcion (1-3)"))
+
+            if opcion == 1:
+                puntos_sumar = -1
+
+                while puntos_sumar < 0:
+                    entrada_puntos = float(input("\n Ingrese la nueva cantidad de puntos (enteros >= 0)"))
+
+                
+
+
+
+
+
